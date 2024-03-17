311baaaab6880c6708798f8a2f57212643deb81f
Karan Gorai <92878327+krn563@users.noreply.github.com>
2024-03-17 19:13:31 +0530

    Navbar Issue and Meteors Overflow and Add (#23)

--- app/MeteorsCard/Page.tsx
+++ app/MeteorsCard/Page.tsx
@@ -3,8 +3,8 @@ import { Meteors } from "../../components/ui/meteors";
 
 export function MeteorsDemo() {
     return (
-      <div className="">
-        <div className="w-full relative max-w-xs">
+      <div className="flex items-center justify-evenly">
+        <div className="w-full relative max-w-sm px-8 py-4 min-w-64">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
           <div className="relative shadow-xl bg-gray-900 border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
             <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
@@ -49,7 +49,7 @@ export function MeteorsDemo() {
   // Duplicate MeteorsDemo component
   export function TripleMeteorsDemo() {
     return (
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 content-center items-center place-content-center px-8">
         <MeteorsDemo />
         <MeteorsDemo />
         <MeteorsDemo />
