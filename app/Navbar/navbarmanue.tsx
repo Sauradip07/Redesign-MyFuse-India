1970cf41242579dafc84c9e532b71e1dffa5e11d
Sudip Mukherjee <sudipmukherjee96144@gmail.com>
2024-03-17 08:06:28 +0530

    fix the glob

--- app/Navbar/navbarmanue.tsx
+++ app/Navbar/navbarmanue.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { navigationMenuTriggerStyle } from "@/components/ui/navbar-menu";
-
 
 import {
   NavigationMenu,
@@ -13,8 +12,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-
-} from "@/components/ui/navbar-menu"
+} from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -52,41 +50,41 @@ const components: { title: string; href: string; description: string }[] = [
     description:
       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
   },
-]
+];
 export function NavbarDemo() {
-   return (
-      <nav className="bg-white dark:bg-black fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-900">
-         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a
-               href="https://www.myfuse.in/home"
-               className="flex items-center space-x-3 rtl:space-x-reverse">
-               <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  className="h-8"
-                  alt="Flowbite Logo"
-               />
-               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  MyFuse
-               </span>
-            </a>
-            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-               <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  Get started
-               </button>
-            </div>
-            <div
-               className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-               id="navbar-sticky">
-                
-               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:bg-black">
-
-               </ul>
-            </div>
-         </div>
-      </nav>
-   );
+  return (
+    <nav className="bg-white dark:bg-black fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://www.myfuse.in/home"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            MyFuse
+          </span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Get started
+          </button>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:bg-black"></ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
@@ -110,6 +108,6 @@ const ListItem = React.forwardRef<
         </a>
       </NavigationMenuLink>
     </li>
-  )
-})
-ListItem.displayName = "ListItem"
+  );
+});
+ListItem.displayName = "ListItem";
