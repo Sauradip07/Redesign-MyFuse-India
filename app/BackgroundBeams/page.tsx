35c385c63d505aff7f2e83fcad0b7a27b4817d69
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-03-18 23:00:42 +0530

    Footer fix

--- app/BackgroundBeams/page.tsx
+++ app/BackgroundBeams/page.tsx
@@ -4,8 +4,8 @@ import { BackgroundBeams } from "../../components/ui/background-beams";
 
 export function BackgroundBeamsDemo() {
    return (
-      <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-         <div className="max-w-4xl mx-auto p-4">
+      <div className="h-auto w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+         <div className="max-w-7xl mx-auto p-4">
             <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
                Join Our Newsletter
             </h1>
