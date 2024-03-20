a7d424d1d7c8795f4d828d8dc7a6f479b37b33bf
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-03-20 18:17:51 +0530

    blog  card below section fix

--- components/ui/autoincrase.tsx
+++ components/ui/autoincrase.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import CountUp from 'react-countup';
+import React from "react";
+import CountUp from "react-countup";
 
 const Counter = ({ number, title }) => {
-    return (
-        <div className="number mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <h2 className="counter mb-3 font-normal text-gray-700 dark:text-gray-400">{title}</h2>
-            <CountUp duration={3} end={number} />
-        </div>
-    );
-}
+   return (
+      <div className="number mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+         <h2 className="counter mb-3 text-slate-300 font-semibold">{title}</h2>
+         <CountUp duration={3} end={number} />
+      </div>
+   );
+};
 
 export default Counter;
